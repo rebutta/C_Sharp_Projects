033a2c0798e5d051a8dd6f2f075482d34d5d35fc
rebutta <101282383+rebutta@users.noreply.github.com>
2022-04-27 17:14:11 -0700

    MATH AND COMPARISON OPERATOR SUBMISSION ASSIGNMENT

---
+++ AIC/AIC/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIC
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program"); //console.writeline name of program
+            Console.ReadLine(); //displays the complete string until the user presses the enter key
+
+            //Person 1
+
+            Console.WriteLine("Person 1"); //console.writeline name first user
+            Console.ReadLine(); 
+
+            Console.WriteLine("What is your hourly rate?"); //console.writeline displays question
+            int person1Pay = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many hours worked per week"); //console.writeline displays next question
+            int person1Hours = Convert.ToInt32(Console.ReadLine()); 
+
+            //Person 2
+
+            Console.WriteLine("Person 2");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your hourly rate?");
+            int person2Pay = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine("How many hours worked per week"); 
+            int person2Hours = Convert.ToInt32(Console.ReadLine());
+
+            //Adding annual salary of both persons
+
+            int person1WeekTotal = person1Hours * person1Pay; //one week of pay
+
+            int person2WeekTotal = person2Hours * person2Pay; 
+
+            int person1Annual = person1WeekTotal * 52; //multiply by weeks in a year (52 weeks)
+
+            int person2Annual = person2WeekTotal * 52; 
+
+            Console.WriteLine("Annual salary of Person 1: $" + person1Annual); //displays person1 annual salary
+            Console.ReadLine();
+
+            Console.WriteLine("Annual salary of Person 2: $" + person2Annual); 
+            Console.ReadLine();
+
+            //Boolean comparing salaries
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool bigBaller = person1Annual > person2Annual;
+            Console.WriteLine(bigBaller);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ mathAndComparisonOperators/mathAndComparisonOperators/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine("Five plus Ten = " + total.ToString());
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine("Ten times Five = " + product.ToString());
+            //Console.ReadLine();
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine("Hundred divided Five = " + quotient.ToString());
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine("Hundred divided Seventeen = " + quotient.ToString());
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+
+
+            int roomTemp = 70;
+            int currentTemp = 70;
+
+            //bool isWarm = currentTemp >= roomTemp;
+            bool isWarm = currentTemp != roomTemp;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+        }
+    }
+}
