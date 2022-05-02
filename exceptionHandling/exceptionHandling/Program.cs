37b239b072999820f9b5284ae3a934da906493e1
rebutta <101282383+rebutta@users.noreply.github.com>
2022-05-01 23:06:58 -0700

    added notes

--- exceptionHandling/exceptionHandling/Program.cs
+++ exceptionHandling/exceptionHandling/Program.cs
@@ -8,34 +8,31 @@ namespace exceptionHandling
         {
             try 
             {
-            
-
-
-                Console.WriteLine("Pick a number.");
-                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a number."); // first line asks user to pick a number (int)
+                int numberOne = Convert.ToInt32(Console.ReadLine()); // converts user entered string to int
                 Console.WriteLine("Pick a second number.");
                 int numberTwo = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Dividing the two...");
-                int numberThree = numberOne / numberTwo;
-                Console.WriteLine(numberOne + "divided by " + numberTwo + "equals " + numberThree);
+                int numberThree = numberOne / numberTwo; // dividing user entered int numberOne and numberTwo
+                Console.WriteLine(numberOne + "divided by " + numberTwo + "equals " + numberThree); // displaying the above math
                 Console.ReadLine();
             }
-            catch (FormatException ex)
+            catch (FormatException ex) // catch for non int entry
             {
-                Console.WriteLine("Please type a whole number.");
+                Console.WriteLine("Please type a whole number."); // message displayed for FormatException
                 return;
             }
-            catch (DivideByZeroException ex)
+            catch (DivideByZeroException ex) // catch for dividing by zero
             {
-                Console.WriteLine("Please don't divide by zero.");
+                Console.WriteLine("Please don't divide by zero."); // message displayed for DivideByZeroException
             }
-            catch (Exception ex)
+            catch (Exception ex) // catch for any error not explicitly written above
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message); // message displayed for General Exception
             }
-            finally
+            finally 
             {
-                Console.ReadLine();
+                Console.ReadLine(); //readline to allow user to read error without console ending abruptly 
             }
         }
     }
