48144dba6e5b4e9172941e326d79425099df9106
rebutta <101282383+rebutta@users.noreply.github.com>
2022-05-02 03:04:01 -0700

    CALLING METHODS SUBMISSION ASSIGNMENT

---
+++ callingMethods/callingMethods/Integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callingMethods
+{
+    public class Integer
+    {
+        public int methodOne(int x)
+        {
+            return 5 + x;
+        }
+
+        public int methodTwo(int y)
+        {
+            return 5 * y;
+        }
+
+        public int methodThree(int z)
+        {
+            return 150 - z;
+        }
+
+    }
+}
---
+++ callingMethods/callingMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer integer = new Integer(); //creates a new object from the class Integer 
+
+            Console.WriteLine("What number would you like to see the math operations performed on?");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Five plus " + userNumber + " equals " + integer.methodOne(userNumber)); //calls on first method inputing user int
+            Console.ReadLine();
+
+            Console.WriteLine("Five times " + userNumber + " equals " + integer.methodTwo(userNumber));
+            Console.ReadLine();
+
+            Console.WriteLine("Hundred and fifty minus " + userNumber + " equals " + integer.methodThree(userNumber));
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for your time!");
+            Console.ReadLine();
+        }
+    }
+}
