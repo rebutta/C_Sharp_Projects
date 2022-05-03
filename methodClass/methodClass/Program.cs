84fc60b2ce2891ee971a3a88e8d7f978bf3b25fa
rebutta <101282383+rebutta@users.noreply.github.com>
2022-05-02 22:08:24 -0700

    METHOD CLASS SUBMISSION ASSIGNMENT

---
+++ methodClass/methodClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            voidMethod voidmethod = new voidMethod();
+            int j = 5;
+            int p = 100;
+
+            voidmethod.methodOne(j, p);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodClass/methodClass/voidMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodClass
+{
+    public class voidMethod
+    {
+        public void methodOne(int x, int y)
+        {
+            Convert.ToInt32(5 + x);
+            Console.WriteLine(y);
+        }
+
+        
+    }
+}
