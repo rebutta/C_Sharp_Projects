933bc43fe49ceccdd13866e61b939ea646122108
rebutta <101282383+rebutta@users.noreply.github.com>
2022-05-02 17:29:49 -0700

    update

--- mainMethodApp/mainMethodApp/Program.cs
+++ mainMethodApp/mainMethodApp/Program.cs
@@ -10,6 +10,7 @@ namespace mainMethodApp
     {
         static void Main(string[] args)
         {
+            // main method submission 
             Integer integer = new Integer(); //creates a new object from the class Integer
 
             Console.WriteLine(integer.methodOne(10));
@@ -20,6 +21,16 @@ namespace mainMethodApp
 
             Console.WriteLine(integer.methodOne("8"));
             Console.ReadLine();
-        }
+
+            // method submission 
+            Integer2 integer2 = new Integer2();
+            Console.WriteLine("Please choose the first of one to two numbers to use in my method.");
+            int pickA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("NOT required but you can choose the second of two numbers to use in my method.");
+            int pickB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(integer2.methodUno(pickA, pickB));
+            Console.ReadLine();
+        }   
     }
 }
---
+++ mainMethodApp/mainMethodApp/integer2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethodApp
+{
+    public class Integer2
+    {
+        public int methodUno(int x, int y)
+        {
+            return (5 + x) * y; //how do i make this optional!!!!!!?????
+        }
+    }
+}
